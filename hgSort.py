02a0067e5125f2905431d72947069eb818f67619
logArythm557 <jake.ewhite@gmail.com>
2018-07-09 16:52:15 +0930

    Update hgSort.py
    semi functional

--- hgSort.py
+++ hgSort.py
@@ -1,31 +1,36 @@
-#hand-grenade sort, a near-enough is good enough implementation of quicksort
+# hand-grenade sort, a near-enough is good enough implementation of quicksort
 import random
+
 randlist = []
 
 for x in range(10):
-	randlist.append(random.randint(1,51))
+    randlist.append(random.randint(1, 51))
 
-def hgSort(list):
-	l = []
-	e = []
-	g = []
-	hg = []
-	out = []
 
-	if len(list) >1:
-		pivot = list[0]
-		for x in list:
-			if x < pivot and random.randint(0,101) < 95:
-				l.append(x)
-			if x == pivot:
-				e.append(x)
-			if x > pivot and random.randint(0,101) < 95:
-				g.append(x)
-			else: hg.append(x)
-		out = hgSort(l)+e+hgSort(g)
+def hgSort(inlist):
+    l = []
+    e = []
+    g = []
+    hg = []
 
-		for x in hg:
-			out.insert(random.randint(0,len(out)),x)
-		print out
-	else:
-		return list
+    if len(inlist) > 1:
+        pivot = inlist[0]
+        for x in inlist:
+            if x < pivot and random.randint(0, 101) < 95:
+                l.append(x)
+            if x == pivot:
+                e.append(x)
+            if x > pivot and random.randint(0, 101) < 95:
+                g.append(x)
+            elif len(hg) > 0:
+                hg.append(x)
+        out = hgSort(l) + e + hgSort(g)
+
+        for x in hg:
+            out.insert(random.randint(0, len(out)), x)
+        return out
+    else:
+        return inlist
+
+
+print hgSort(randlist)
